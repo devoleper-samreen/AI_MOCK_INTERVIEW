609d293d4e5a03ad5684bb4e98138b0a37d228dc
devoleper-samreen <samreenmalik52292@gmail.com>
2025-07-29 13:06:20 +0530

    feat: initialize project with React, Tailwind CSS, shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="bg-blue-200">
+      <Button variant={"outline"} size={"icon"}>
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
