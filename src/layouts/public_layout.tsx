5fe86f82c1deabec2222d3a518a0eee23832da1f
devoleper-samreen <samreenmalik52292@gmail.com>
2025-07-30 10:50:52 +0530

    feat: implement AuthHandler for user authentication and data storage

--- src/layouts/public_layout.tsx
+++ src/layouts/public_layout.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
+import AuthHandler from "@/handlers/user_auth_handler";
 
 const PublicLayout = () => {
   return (
     <div className="w-full">
+      <AuthHandler />
       <Header />
       <Outlet />
       <Footer />
