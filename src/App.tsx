e64e55773cf832917e3bb5a18b025881e33f5484
devoleper-samreen <samreenmalik52292@gmail.com>
2025-08-14 19:16:11 +0530

    add console

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,15 @@ import { MockLoadPage } from "./routes/MockLoadPage";
 import { MockInterviewPage } from "./routes/MockInterviewPage";
 import { Feedback } from "./routes/Feedback";
 import { InterviewHistory } from "./routes/InterviewHistory";
+import { useEffect } from "react";
 function App() {
+  console.log("Firebase Key:", import.meta.env.VITE_FIREBASE_API_KEY);
+  console.log("Gemini Key:", import.meta.env.VITE_GEMINI_API_KEY);
+
+  useEffect(() => {
+    console.log("Firebase Key:", import.meta.env.VITE_FIREBASE_API_KEY);
+    console.log("Gemini Key:", import.meta.env.VITE_GEMINI_API_KEY);
+  }, []);
   return (
     <Router>
       <Routes>
